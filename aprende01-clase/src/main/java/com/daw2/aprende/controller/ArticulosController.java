1d68a927361bc7c0ca17e95c2b9665e566b6eb0c
AlexFeliz64 <aalonsos21@iescastelar.com>
2024-12-11 17:22:50 +0100

    Nuevo proyecto viajes-clase

--- aprende01-clase/src/main/java/com/daw2/aprende/controller/ArticulosController.java
+++ aprende01-clase/src/main/java/com/daw2/aprende/controller/ArticulosController.java
@@ -19,6 +19,7 @@ import java.util.List;
 @Slf4j
 @RestController
 @RequestMapping("/arti")
+@CrossOrigin("*")
 public class ArticulosController {
 
     @Autowired
--- aprende01-clase/src/main/java/com/daw2/aprende/controller/ProveedoresController.java
+++ aprende01-clase/src/main/java/com/daw2/aprende/controller/ProveedoresController.java
@@ -19,6 +19,7 @@ import java.util.List;
 @Slf4j
 @RestController
 @RequestMapping("/pro")
+@CrossOrigin("*")
 public class ProveedoresController {
 
 
